ea72ae67dcdb67f25c2fbcdbba144b38331fc689
Kwang Eun Jang <kejang@stanford.edu>
2025-02-21 18:14:37 -0800

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ package_name = "splitbregman"
 try:
     pkg_version = version(package_name)
 except Exception:
-    pkg_version = "0.0.1"
+    pkg_version = "0.0.2"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
